e081c4d8b76343b0720a00dde0c82c5485352b8a
Ravikumar <rkumbalore@apple.com>
2019-10-18 15:05:50 -0700

    Created new packages per topics

---
+++ src/com/corejava/sorting/QuickSortProgramming.java
@@ -0,0 +1,26 @@
+package com.corejava.programming;
+
+public class QuickSortProgramming {
+
+	public static void main(String[] args) {
+		int orgArray[] = new int[] { 9, 2, 5, 6, 4, 3, 7, 10, 1, 12, 8, 11 };
+		int lowerArray[] = null;
+		int higherArray[]= null;
+		int pivot = orgArray[6];
+		
+		System.out.println("The pivot element is :"+pivot);
+
+		for (int i = 0; i < orgArray.length; i++) {
+			if(orgArray[i]<pivot){
+				lowerArray[i]=orgArray[i];
+			}else{
+				higherArray[i]=orgArray[i];
+			}
+		}
+		
+		System.out.println("Lower Array : "+lowerArray);
+		System.out.println("Higher Array : "+higherArray);
+
+	}
+
+}

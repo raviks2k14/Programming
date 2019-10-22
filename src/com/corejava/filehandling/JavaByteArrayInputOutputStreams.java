35564a7e95de019ae470b57dde67023217e6c971
Ravikumar <rkumbalore@apple.com>
2019-10-22 14:59:58 -0700

    Added files

---
+++ src/com/corejava/filehandling/JavaByteArrayInputOutputStreams.java
@@ -0,0 +1,39 @@
+package com.corejava.filehandling;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class JavaByteArrayInputOutputStreams {
+
+	public static void main(String[] args) throws IOException {
+		// The ByteArrayInputStream class allows a buffer in the memory to be
+		// used as an InputStream. The input source is a byte array.
+		ByteArrayOutputStream bOutput = new ByteArrayOutputStream(5);
+
+		while (bOutput.size() < 5) {
+			bOutput.write("hello".getBytes());
+		}
+
+		// byte b[] = "hello".getBytes(); --> This is the easy way
+		byte b[] = bOutput.toByteArray();
+
+		System.out.println("Print the content");
+
+		for (int x = 0; x < b.length; x++) {
+			// printing the characters
+			System.out.print((char) b[x] + "   ");
+		}
+		System.out.println("   ");
+
+		int c;
+		ByteArrayInputStream bInput = new ByteArrayInputStream(b);
+		System.out.println("Converting characters to Upper case ");
+
+		while ((c = bInput.read()) != -1) {
+			System.out.println(Character.toUpperCase((char) c));
+		}
+
+	}
+
+}
---
+++ src/com/corejava/filehandling/JavaDataInputOutputStreams.java
@@ -0,0 +1,43 @@
+package com.corejava.filehandling;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class JavaDataInputOutputStreams {
+
+	public static void main(String[] args) throws IOException {
+
+		DataOutputStream dOutput = new DataOutputStream(
+				new FileOutputStream(
+						"/Users/ravikumarks/Desktop/Reading/filesforprograms/input.txt"));
+		DataInputStream dataIn = null;
+		try {
+			dOutput.writeUTF("Hello");
+
+			// Reading data from the same file
+			dataIn = new DataInputStream(
+					new FileInputStream(
+							"/Users/ravikumarks/Desktop/Reading/filesforprograms/input.txt"));
+			while (dataIn.available() > 0) {
+				String k = dataIn.readUTF();
+				System.out.print(k + " ");
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(dataIn != null){
+				dataIn.close();
+			}
+			if(dOutput != null){
+				dOutput.close();
+			}
+		}
+
+	}
+
+}

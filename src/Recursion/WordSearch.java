8a6f9a02f3987fad2b9984cddc2f76a37588858a
ravi2k14tech <ravi2k14tech@gmail.com>
2021-02-05 22:28:45 -0800

    Word Search

---
+++ src/Recursion/WordSearch.java
@@ -0,0 +1,51 @@
+package Recursion;
+
+public class WordSearch {
+
+	public boolean exist(char[][] board, String word) {
+		int rows = board.length;
+		int columns = board[0].length;
+		boolean result = false;
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (dfs(board, word, i, j, 0)) {
+					result = true;
+				}
+			}
+		}
+
+		return result;
+
+	}
+
+	public boolean dfs(char[][] board, String word, int i, int j, int k) {
+		int rows = board.length;
+		int columns = board[0].length;
+
+		if (i < 0 || j < 0 || i >= rows || j >= columns) {
+			return false;
+		}
+
+		if (board[i][j] == word.charAt(k)) {
+			char temp = board[i][j];
+			board[i][j] = '#';
+			if (k == word.length() - 1) {
+				return true;
+			} else if (dfs(board, word, i - 1, j, k + 1) || dfs(board, word, i + 1, j, k + 1)
+					|| dfs(board, word, i, j - 1, k + 1) || dfs(board, word, i, j + 1, k + 1)) {
+				return true;
+			}
+			//board[i][j] = temp;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		WordSearch wsObj = new WordSearch();
+		char[][] board = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+		System.out.println(wsObj.exist(board, "SFCE"));
+	}
+
+}
